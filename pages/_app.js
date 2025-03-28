623ef2d3a07ec2d67eb6e39a3f021782ea839d87
akshaykhatrinm <166972697+akshaykhatrinm@users.noreply.github.com>
2025-03-28 17:22:57 +0530

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import '../styles/dashboard.css';  // Global CSS import
+import { useState, useEffect } from 'react';
+import { parseCookies } from 'nookies';
+
+// Your other custom App logic here
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
